195e7fed6a5f6c447dc5a49fb7ae7731c404a89d
matheusrmello <matheus.rm.09@hotmail.com>
2024-08-09 11:25:56 -0300

    Create infra

--- infra/EKS.tf
+++ infra/EKS.tf
@@ -2,7 +2,7 @@ module "eks" {
   source = "terraform-aws-modules/eks/aws"
 
   cluster_name                    = var.cluster_name
-  cluster_version                 = "1.24"
+  cluster_version                 = "1.25"
   cluster_endpoint_public_access  = true
 
   vpc_id     = module.vpc.vpc_id
